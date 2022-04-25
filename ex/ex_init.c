19c8c0fc24312feda74919babd6db19f347fb20a
Jeremy Mates <jeremy.mates@gmail.com>
2022-04-25 09:29:01 -0700

    read ~/.exrc, read EXINIT
    and toss a whole lot of code along the way

--- ex/ex_init.c
+++ ex/ex_init.c
@@ -28,7 +28,7 @@
 #include "pathnames.h"
 
 enum rc { NOEXIST, NOPERM, RCOK };
-static enum rc	exrc_isok(SCR *, struct stat *, int *, char *, int, int);
+static enum rc	exrc_isok(SCR *, struct stat *, int *, char *);
 
 static int ex_run_file(SCR *, int, char *);
 
@@ -124,6 +124,9 @@ ex_optchange(SCR *sp, int offset, char *str, u_long *valp)
  *	Read the EXINIT environment variable and the startup exrc files,
  *	and execute their commands.
  *
+ * !!!
+ * this has been much simplified -- read ~/.exrc, read EXINIT
+ *
  * PUBLIC: int ex_exrc(SCR *);
  */
 int
@@ -133,38 +136,16 @@ ex_exrc(SCR *sp)
 	char *p, path[PATH_MAX];
 	int fd;
 
-	/*
-	 * Source the system, environment, $HOME and local .exrc values.
-	 * Vi historically didn't check $HOME/.exrc if the environment
-	 * variable EXINIT was set.  This is all done before the file is
-	 * read in, because things in the .exrc information can set, for
-	 * example, the recovery directory.
-	 *
-	 * !!!
-	 * While nvi can handle any of the options settings of historic vi,
-	 * the converse is not true.  Since users are going to have to have
-	 * files and environmental variables that work with both, we use nvi
-	 * versions of both the $HOME and local startup files if they exist,
-	 * otherwise the historic ones.
-	 *
-	 * !!!
-	 * For a discussion of permissions and when what .exrc files are
-	 * read, see the comment above the exrc_isok() function below.
-	 *
-	 * !!!
-	 * If the user started the historic of vi in $HOME, vi read the user's
-	 * .exrc file twice, as $HOME/.exrc and as ./.exrc.  We avoid this, as
-	 * it's going to make some commands behave oddly, and I can't imagine
-	 * anyone depending on it.
-	 */
-	switch (exrc_isok(sp, &hsb, &fd, _PATH_SYSEXRC, 1, 0)) {
-	case NOEXIST:
-	case NOPERM:
-		break;
-	case RCOK:
-		if (ex_run_file(sp, fd, _PATH_SYSEXRC))
-			return (1);
-		break;
+	if ((p = getenv("HOME")) != NULL && *p) {
+		(void)snprintf(path, sizeof(path), "%s/%s", p, _PATH_EXRC);
+		switch (exrc_isok(sp, &hsb, &fd, path)) {
+			case NOEXIST:
+			case NOPERM:
+				break;
+			case RCOK:
+				if (ex_run_file(sp, fd, path))
+					return (1);
+		}
 	}
 
 	/* Run the commands. */
@@ -173,29 +154,9 @@ ex_exrc(SCR *sp)
 	if (F_ISSET(sp, SC_EXIT | SC_EXIT_FORCE))
 		return (0);
 
-	if ((p = getenv("NEXINIT")) != NULL) {
-		if (ex_run_str(sp, "NEXINIT", p, strlen(p), 1, 0))
-			return (1);
-	} else if ((p = getenv("EXINIT")) != NULL) {
+	if ((p = getenv("EXINIT")) != NULL) {
 		if (ex_run_str(sp, "EXINIT", p, strlen(p), 1, 0))
 			return (1);
-	} else if ((p = getenv("HOME")) != NULL && *p) {
-		(void)snprintf(path, sizeof(path), "%s/%s", p, _PATH_NEXRC);
-		switch (exrc_isok(sp, &hsb, &fd, path, 0, 1)) {
-		case NOEXIST:
-			(void)snprintf(path,
-			    sizeof(path), "%s/%s", p, _PATH_EXRC);
-			if (exrc_isok(sp, &hsb, &fd, path, 0, 1) == RCOK &&
-			    ex_run_file(sp, fd, path))
-				return (1);
-			break;
-		case NOPERM:
-			break;
-		case RCOK:
-			if (ex_run_file(sp, fd, path))
-				return (1);
-			break;
-		}
 	}
 
 	/* Run the commands. */
@@ -203,38 +164,6 @@ ex_exrc(SCR *sp)
 		(void)ex_cmd(sp);
 	if (F_ISSET(sp, SC_EXIT | SC_EXIT_FORCE))
 		return (0);
-
-	/* Previous commands may have set the exrc option. */
-	if (O_ISSET(sp, O_EXRC)) {
-		switch (exrc_isok(sp, &lsb, &fd, _PATH_NEXRC, 0, 0)) {
-		case NOEXIST:
-			if (exrc_isok(sp, &lsb, &fd, _PATH_EXRC, 0, 0)
-			    == RCOK) {
-				if (lsb.st_dev != hsb.st_dev ||
-				    lsb.st_ino != hsb.st_ino) {
-					if (ex_run_file(sp, fd, _PATH_EXRC))
-						return (1);
-				} else
-					close(fd);
-			}
-			break;
-		case NOPERM:
-			break;
-		case RCOK:
-			if (lsb.st_dev != hsb.st_dev ||
-			    lsb.st_ino != hsb.st_ino) {
-				if (ex_run_file(sp, fd, _PATH_NEXRC))
-					return (1);
-			} else
-				close(fd);
-			break;
-		}
-		/* Run the commands. */
-		if (EXCMD_RUNNING(sp->gp))
-			(void)ex_cmd(sp);
-		if (F_ISSET(sp, SC_EXIT | SC_EXIT_FORCE))
-			return (0);
-	}
 
 	return (0);
 }
@@ -301,117 +230,19 @@ ex_run_str(SCR *sp, char *name, char *str, size_t len, int ex_flags,
  *	Open and check a .exrc file for source-ability.
  *
  * !!!
- * Historically, vi read the $HOME and local .exrc files if they were owned
- * by the user's real ID, or the "sourceany" option was set, regardless of
- * any other considerations.  We no longer support the sourceany option as
- * it's a security problem of mammoth proportions.  We require the system
- * .exrc file to be owned by root, the $HOME .exrc file to be owned by the
- * user's effective ID (or that the user's effective ID be root) and the
- * local .exrc files to be owned by the user's effective ID.  In all cases,
- * the file cannot be writeable by anyone other than its owner.
- *
- * In O'Reilly ("Learning the VI Editor", Fifth Ed., May 1992, page 106),
- * it notes that System V release 3.2 and later has an option "[no]exrc".
- * The behavior is that local .exrc files are read only if the exrc option
- * is set.  The default for the exrc option was off, so, by default, local
- * .exrc files were not read.  The problem this was intended to solve was
- * that System V permitted users to give away files, so there's no possible
- * ownership or writeability test to ensure that the file is safe.
- *
- * POSIX 1003.2-1992 standardized exrc as an option.  It required the exrc
- * option to be off by default, thus local .exrc files are not to be read
- * by default.  The Rationale noted (incorrectly) that this was a change
- * to historic practice, but correctly noted that a default of off improves
- * system security.  POSIX also required that vi check the effective user
- * ID instead of the real user ID, which is why we've switched from historic
- * practice.
- *
- * We initialize the exrc variable to off.  If it's turned on by the system
- * or $HOME .exrc files, and the local .exrc file passes the ownership and
- * writeability tests, then we read it.  This breaks historic 4BSD practice,
- * but it gives us a measure of security on systems where users can give away
- * files.
+ * This has been very much simplified on the assumption that files
+ * cannot be "given away" (SysV crazy?) and that the user knows
+ * how to manage the permissions of their dotfiles.
  */
 static enum rc
-exrc_isok(SCR *sp, struct stat *sbp, int *fdp, char *path, int rootown,
-    int rootid)
+exrc_isok(SCR *sp, struct stat *sbp, int *fdp, char *path)
 {
-	enum { ROOTOWN, OWN, WRITER } etype;
-	uid_t euid;
-	int nf1, nf2;
-	char *a, *b, buf[PATH_MAX];
-
-	if ((*fdp = open(path, O_RDONLY)) < 0) {
+	if ((*fdp = open(path, O_RDONLY, 0)) < 0) {
 		if (errno == ENOENT)
-                        /* This is the only case where ex_exrc()
-                         * should silently try the next file, for
-                         * example .exrc after .nexrc.
-			 */
 			return (NOEXIST);
 
 		msgq_str(sp, M_SYSERR, path, "%s");
 		return (NOPERM);
 	}
-
-	if (fstat(*fdp, sbp)) {
-		msgq_str(sp, M_SYSERR, path, "%s");
-		close(*fdp);
-		return (NOPERM);
-	}
-
-	/* Check ownership permissions. */
-	euid = geteuid();
-	if (!(rootown && sbp->st_uid == 0) &&
-	    !(rootid && euid == 0) && sbp->st_uid != euid) {
-		etype = rootown ? ROOTOWN : OWN;
-		goto denied;
-	}
-
-	/* Check writeability. */
-	if (sbp->st_mode & (S_IWGRP | S_IWOTH)) {
-		etype = WRITER;
-		goto denied;
-	}
 	return (RCOK);
-
-denied:	a = msg_print(sp, path, &nf1);
-	if (strchr(path, '/') == NULL && getcwd(buf, sizeof(buf)) != NULL) {
-		b = msg_print(sp, buf, &nf2);
-		switch (etype) {
-		case ROOTOWN:
-			msgq(sp, M_ERR,
-			    "%s/%s: not sourced: not owned by you or root",
-			    b, a);
-			break;
-		case OWN:
-			msgq(sp, M_ERR,
-			    "%s/%s: not sourced: not owned by you", b, a);
-			break;
-		case WRITER:
-			msgq(sp, M_ERR,
-    "%s/%s: not sourced: writable by a user other than the owner", b, a);
-			break;
-		}
-		if (nf2)
-			FREE_SPACE(sp, b, 0);
-	} else
-		switch (etype) {
-		case ROOTOWN:
-			msgq(sp, M_ERR,
-			    "%s: not sourced: not owned by you or root", a);
-			break;
-		case OWN:
-			msgq(sp, M_ERR,
-			    "%s: not sourced: not owned by you", a);
-			break;
-		case WRITER:
-			msgq(sp, M_ERR,
-	    "%s: not sourced: writable by a user other than the owner", a);
-			break;
-		}
-
-	if (nf1)
-		FREE_SPACE(sp, a, 0);
-	close(*fdp);
-	return (NOPERM);
 }
